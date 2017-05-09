fc30fb7400a4f0e78d6ddca4c86285824d899185
janibasha <janibasha@ladatechnologies.com>
2017-05-09 17:49:34 +0530

    created home and login pages

---
+++ application/controllers/Admin.php
@@ -0,0 +1,23 @@
+<?php
+
+//error_reporting(E_ALL);
+//ini_set("display_errors", 1);
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+    function __construct() {
+
+        parent::__construct();
+        $this->load->library('session', 'form_validation', 'email');
+        $this->load->helper('url', 'form', 'html');
+        $this->load->library('pagination');
+}          
+
+    public function index() {
+       
+        $this->load->view('home');
+    }   
+}
+
+?>
---
+++ application/controllers/Index.php
@@ -0,0 +1,21 @@
+<?php
+//error_reporting(E_ALL);
+//ini_set("display_errors", 1);
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Index extends CI_Controller {
+    function __construct() {
+        parent::__construct();
+        $this->load->library('session', 'form_validation', 'email');
+        $this->load->helper('url', 'form', 'html', 'cookie');       
+    }
+
+    public function index() {
+// Load our view to be displayed        
+        $this->load->view('login');
+    }
+     public function home() {
+// Load our view to be displayed        
+        $this->load->view('home');
+    }
+}
